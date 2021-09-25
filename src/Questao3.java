c65dbaa6cc74aaa39bbcd17b8025c6b1cdbc0c95
Lucas Coacci <coacci_lucas@hotmail.com>
2021-09-24 21:49:16 -0300

    Added Questao3.java

---
+++ src/Questao3.java
@@ -0,0 +1,11 @@
+public class Questao3 {
+    public static void main(String[] args) {
+        int i = 0;
+        while (i < 10) { // <- Erro: Falta de um i++ dentro do while para incrementar o valor de i,
+                         // fazendo com que o programa caia
+                         // em um loop infinito
+            System.out.println("Hello World\n");
+        }
+
+    }
+}
